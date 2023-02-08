e47808ec76876c43f882fd9a9d17d6b7bb1b84e2
brad@smithwebtek.com <brad@smithwebtek.com>
2023-02-07 20:22:05 -0500

    Add test for transformToNumber

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -1,9 +1,16 @@
 // import { test} from 'vitest'
 import { it, expect } from 'vitest'
 import { add } from './math'
+import { transformToNumber } from './util/numbers';
 
 it('should summarize all numbers in an array', () => {
   const result = add([1,2,3])
 
   expect(result).toBe(6);
+});
+
+it('should transform a string into a number', () => {
+  const result = transformToNumber('3')
+
+  expect(result).toBe(3)
 });
