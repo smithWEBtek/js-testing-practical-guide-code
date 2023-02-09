687a596ff5df71d396d898f8f6a174fc54fd2cbc
brad@smithwebtek.com <brad@smithwebtek.com>
2023-02-08 20:27:48 -0500

    Add tests 230308

--- code/basic-testing-frontend/src/util/validation.js
+++ code/basic-testing-frontend/src/util/validation.js
@@ -1,6 +1,8 @@
 export function validateStringNotEmpty(value) {
   if (value.trim().length === 0) {
     throw new Error('Invalid input - must not be empty.');
+  } else {
+    return "String OK"
   }
 }
 
