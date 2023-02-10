95a1a8d29e551dbcb464be939c715106f6cbae71
brad@smithwebtek.com <brad@smithwebtek.com>
2023-02-09 21:39:25 -0500

    Add tests 230209

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -2,7 +2,7 @@
 import { it, expect } from 'vitest'
 import { add } from './math'
 import { transformToNumber } from './util/numbers';
-import { validateStringNotEmpty } from './util/validation';
+import { validateStringNotEmpty, validateNumber } from './util/validation';
 
 it('should summarize all numbers in an array', () => {
   const result = add([1,2,3])
@@ -23,4 +23,15 @@ it('should throw an error if string is empty', () => {
 it('should return OK for non-empty string', () => {
   const result = validateStringNotEmpty('asdf')
   expect(result).toBe("String OK")  
+});
+
+it('should throw an error if the variable is not a number', () => {
+  expect(() => {
+    validateNumber('a')
+  }).toThrow("Invalid number input.");
+});
+
+it('should NOT throw an error if the variable is a number', () => {
+  const result = validateNumber(3)
+  expect(result).toBe(undefined)
 });
